d14b0e7fe51fceee6ede13aa31058e57e7bf847d
turrnut <104261407+turrnut@users.noreply.github.com>
2022-08-26 09:06:41 -0400

    Language shell, type code from terminal

---
+++ ushell.py
@@ -0,0 +1,29 @@
+import sys
+import intrepreter as lang
+
+try:
+
+  print(f"""
+The Ur programming language
+Version: {lang.version}
+""")
+  while True:
+    enter = input("arkShell $ ")
+      if enter == "quit":
+          sys.exit(0)
+
+      result, error = lang.run("shell.ark", enter)
+      if error :
+          print(error)
+      else:
+          print(result.value)
+
+            
+except Exception as e:
+    print("Intrepretation Failed", e)
+    sys.exit(0)
+except KeyboardInterrupt as k :
+    print("Enter \"quit\" to exit.")
+except:
+    print("Intrepretation failed due to unknown error")
+    sys.exit(0)
