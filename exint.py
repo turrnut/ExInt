e89d6cc5bb8542220efaf0a51e447bf90db6785f
turrnut <104261407+turrnut@users.noreply.github.com>
2022-09-24 22:32:32 -0400

    Create exint.py

---
+++ exint.py
@@ -0,0 +1,26 @@
+from logging.handlers import DatagramHandler
+import sys
+import interpreter as lang
+try:
+    args = sys.argv
+    if len(args) == 2 :
+        with open(args[1], "r") as f:
+            data = f.read()
+        dataarr = data.split("\n")
+        reslist = []
+        for d in dataarr:
+            result, e = lang.run(args[1], d)
+            if e:
+                print(e)
+                sys.exit(1)
+            reslist.append(result.value)
+        for val in reslist:
+            print(val)
+except Exception as e:
+    print("Interpretation Failed", e)
+    sys.exit(0)
+except KeyboardInterrupt as k :
+    print("Interpretation interrupted")
+except:
+    print("Interpretation failed due to unknown error")
+    sys.exit(0)
